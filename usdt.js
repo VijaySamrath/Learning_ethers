8e5a9bcf612da6f6c41fe808e87160c18f6d1bce
vijay samrath <vijaysamrath462@gmail.com>
2023-01-18 13:48:54 +0530

    Modified usdt.js

--- usdt.js
+++ usdt.js
@@ -5,18 +5,27 @@ require("dotenv").config();
 async function main() {
     const usdtAddress = "0xdAC17F958D2ee523a2206206994597C13D831ec7";
 
-    const provider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/79e94aea484649b1bd26c79c144cd054");
+    const provider = new ethers.providers.JsonRpcProvider(process.env.MainnetinfuraApi);
 
     const contract = new ethers.Contract(usdtAddress, usdtABI, provider)
-    
-    const LatestBlock = await provider.getBlockNumber() // to get the Latest Block
 
-    const transferEvents = await contract.queryFilter('Transfer', 16432107,LatestBlock)
-    console.log(transferEvents)
+    //**to get the past events from a particular Block to the latest Block */
+    // const LatestBlock = await provider.getBlockNumber() // to get the Latest Block
 
-    
+    // const transferEvents = await contract.queryFilter('Transfer', 16432107,LatestBlock)
+    // console.log(transferEvents)
 
+    //**Getting transfer events which transfer valueis  more than 1 usdt 
 
+    contract.on("Transfer", (from , to , value, event) => {
+        let info ={
+            from: from,
+            to: to,
+            value: ethers.utils.formatUnits(value, 7),
+            data: event
+        };
+        console.log(JSON.stringify(info, null, 4))
+    })
 
 }
 main();
